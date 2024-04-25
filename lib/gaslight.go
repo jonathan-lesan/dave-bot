98b4c9751117f6f867a6d042e54b5534f8ec4b98
Jonathan Lesan <jonathan.lesan@gmail.com>
2024-04-25 16:56:58 -0700

    separated files into their own lib package

---
+++ lib/gaslight.go
@@ -0,0 +1,28 @@
+package lib
+
+import (
+	"math/rand"
+)
+
+func SendGaslight() string {
+	array_name := []string{
+		"I never said that.",
+		"I did that because I love you.",
+		"I don't know why you're making such a huge deal of this.",
+		"You're being overly sensitive.",
+		"You are being dramatic.",
+		"You are the issue, not me.",
+		"You are crazy.",
+		"You're being delusional.",
+		"You are just insecure.",
+		"You are so selfish if you don't do this for me.",
+		"You're imagining things.",
+		"You made me do that.",
+		"You don't really feel that way.",
+		"That never happened.",
+		"It's not that big a deal.",
+		"You're just being paranoid.",
+	}
+
+	return array_name[rand.Intn(len(array_name))]
+}
