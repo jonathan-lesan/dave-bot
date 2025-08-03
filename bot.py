c0834184e2b77eca9fd6b6e40bc70532389e1e6f
Jonathan Lesan <jonathan.lesan@gmail.com>
2025-08-03 07:00:03 -0700

    adding python related files

---
+++ bot.py
@@ -0,0 +1,35 @@
+import discord
+import os
+import src.gaslight as gaslight
+import src.help as help
+import src.magic as magic
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower().startswith('!gaslight'):
+        await message.channel.send(gaslight.gaslight())
+
+    if message.content.lower().startswith('!card'):
+        await message.channel.send(magic.card())
+    
+    if message.content.lower().startswith('!help'):
+        await message.channel.send(help.helper_message())
+
+client.run(TOKEN)
---
+++ src/gaslight.py
@@ -0,0 +1,23 @@
+import random
+
+def gaslight():
+    random_array = [
+        "I never said that.",
+		"I did that because I love you.",
+		"I don't know why you're making such a huge deal of this.",
+		"You're being overly sensitive.",
+		"You are being dramatic.",
+		"You are the issue, not me.",
+		"You are crazy.",
+		"You're being delusional.",
+		"You are just insecure.",
+		"You are so selfish if you don't do this for me.",
+		"You're imagining things.",
+		"You made me do that.",
+		"You don't really feel that way.",
+		"That never happened.",
+		"It's not that big a deal.",
+		"You're just being paranoid."
+    ]
+
+    return random.choice(random_array)
